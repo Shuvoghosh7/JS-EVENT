472682afed85712e7b17c0179d274c3d1019a376
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-08 01:21:26 +0600

    use onclick

---
+++ js/event.js
@@ -0,0 +1,8 @@
+function makeRed(){
+    document.body.style.backgroundColor='red';
+}
+const blue = document.getElementById('bluebutton')
+blue.onclick=makeBlue;
+function makeBlue(){
+    document.body.style.backgroundColor='blue';
+}
